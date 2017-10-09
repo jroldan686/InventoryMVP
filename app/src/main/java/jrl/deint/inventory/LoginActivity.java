1fc4acb920f2dcf9e0fc393eb20b72ac14a4084b
jroldan686 <jroldan686@gmail.com>
2017-10-09 13:21:55 +0200

    Initial

---
+++ app/src/main/java/jrl/deint/inventory/LoginActivity.java
@@ -0,0 +1,30 @@
+package jrl.deint.inventory;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView txvSignUp;
+    private Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txvSignUp = (TextView)findViewById(R.id.txvSignUp);
+        txvSignUp.setPaintFlags(txvSignUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
