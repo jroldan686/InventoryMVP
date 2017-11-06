e7e05198bcf2f900ce89e6d25696d5c0514aacb9
jroldan686 <jroldan686@gmail.com>
2017-11-06 12:58:50 +0100

    inventoryJRL171106

--- app/src/main/java/jrl/deint/inventory/AddDependencyActivity.java
+++ app/src/main/java/jrl/deint/inventory/AddDependencyActivity.java
@@ -1,14 +1,24 @@
 package jrl.deint.inventory;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import jrl.deint.inventory.pojo.Dependency;
+import jrl.deint.inventory.repository.DependencyRepository;
 
 public class AddDependencyActivity extends AppCompatActivity {
 
+    TextInputEditText tietName;
+    TextInputEditText tietShortName;
+    TextInputEditText tietDescription;
     FloatingActionButton fab;
+    DependencyRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +26,23 @@ public class AddDependencyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_dependency);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        tietName = (TextInputEditText)findViewById(R.id.tietName);
+        tietShortName = (TextInputEditText)findViewById(R.id.tietShortName);
+        tietDescription = (TextInputEditText)findViewById(R.id.tietDescription);
         fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // VALIDA LOS DATOS
-                // AGREGA A REPOSITORY
+                String name = tietName.getText().toString();
+                String shortname = tietShortName.getText().toString();
+                String description = tietDescription.getText().toString();
+
+                if(!(name.isEmpty() || shortname.isEmpty() || description.isEmpty()))
+                    // AGREGA A REPOSITORY
+                    repository.addDependency(new Dependency(3, name, shortname, description));
+                else
+                    Toast.makeText(AddDependencyActivity.this, "Los campos no pueden estar vacíos. Por favor, revíselos.", Toast.LENGTH_SHORT).show();
             }
         });
     }
