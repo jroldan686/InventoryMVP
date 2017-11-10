13b12686d30cbeb71a8c87dca276348ab8f21e8e
jroldan686 <jroldan686@gmail.com>
2017-11-10 13:51:38 +0100

    inventoryJRL171110

--- app/src/androidTest/java/jrl/deint/inventory/LoginInstrumentedTest.java
+++ app/src/androidTest/java/jrl/deint/inventory/LoginInstrumentedTest.java
@@ -1,5 +1,7 @@
 package jrl.deint.inventory;
 
+import android.support.annotation.StringRes;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -8,11 +10,21 @@ import org.mockito.Mock;
 
 import jrl.deint.inventory.repository.UserRepository;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.mockito.Mockito.mock;
 
 /**
  * Instrumentation test, which will execute on an Android device.
  *
+ * @author Jesús Roldán López
+ * @version 1.0
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  * 1. El usuario introduce usuario/email (Instrumentales)
  * 2. El usuario introduce contraseña (Instrumentales)
@@ -31,7 +43,8 @@ public class LoginInstrumentedTest {
      */
     @Test
     public void isUserEmpty() {
-
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorUserEmpty);
     }
 
     /**
@@ -39,16 +52,17 @@ public class LoginInstrumentedTest {
      */
     @Test
     public void isEmailEmpty() {
-
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorEmailEmpty);
     }
-
 
     /**
      * Comprueba que el Password no es vacío
      */
     @Test
     public void isPasswordEmpty() {
-
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorPasswordEmpty);
     }
 
     /**
@@ -56,11 +70,18 @@ public class LoginInstrumentedTest {
      */
     @Test
     public void passwordLength() {
-
+        onView(withId(R.id.edtUser)).perform(typeText("lourdes"), closeSoftKeyboard());
+        onView(withId(R.id.edtPassword)).perform(typeText("lu"), closeSoftKeyboard());
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorPasswordLength);
     }
 
     @Test
     public void passwordDoubleCheck() {
 
+    }
+
+    private void checkSnackBarDisplayByMessage(@StringRes int message) {
+        onView(withText(message)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 }
